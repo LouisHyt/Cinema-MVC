33d824112c3ea39b6002a64b57cc9b949a004aab
Louis | Diox <45403791+LouisHyt@users.noreply.github.com>
2024-12-03 19:39:51 +0100

    Added styles

--- php/view/partials/navbar.php
+++ php/view/partials/navbar.php
@@ -1,3 +1,19 @@
+<link rel="stylesheet" href="public/css/navbar.css">
 <nav id="navbar">
-    
+    <div class="search-container">
+        <input type="text" class="search-input" placeholder="Rechercher un film">
+        <i class="fas fa-search search-icon"></i>
+    </div>
+    <div class="nav-icons">
+        <a href="#" class="nav-icon">
+            <i class="fas fa-bell"></i>
+            <span class="notification-badge">2</span>
+        </a>
+        <a href="#" class="nav-icon">
+            <i class="fas fa-plus"></i>
+        </a>
+        <a href="#" class="nav-icon">
+            <i class="fas fa-user"></i>
+        </a>
+    </div>
 </nav>
--- php/view/partials/sidebar.php
+++ php/view/partials/sidebar.php
@@ -1,3 +1,72 @@
+<link rel="stylesheet" href="public/css/sidebar.css">
 <aside id="sidebar">
-    
+
+    <div class="logo">
+        <span class="cine">CINE</span><span class="plus">PLUS</span>
+    </div>
+    <div class="navigation">
+        <h3>Navigation</h3>
+        <ul class="nav-links">
+            <li>
+                <a href="#" class="active">
+                    <i class="fas fa-film"></i>
+                    <span>Parcourir</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-clock"></i>
+                    <span>Nouveautés</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-fire"></i>
+                    <span>Populaires</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-star"></i>
+                    <span>Mieux notés</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-list"></i>
+                    <span>Catégories</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="actors">
+        <h3>Acteurs</h3>
+        <ul class="actors-list">
+            <li>
+                <img src="public/images/actors/liam-neeson.jpg" alt="Liam Neeson">
+                <span>Liam Neeson</span>
+            </li>
+            <li>
+                <img src="public/images/actors/leonardo-dicaprio.jpg" alt="Leonardo DiCaprio">
+                <span>Leonardo DiCaprio</span>
+            </li>
+            <li>
+                <img src="public/images/actors/marion-cotillard.jpg" alt="Marion Cotillard">
+                <span>Marion Cotillard</span>
+            </li>
+            <li>
+                <img src="public/images/actors/robert-downey-jr.jpg" alt="Robert Downey Jr.">
+                <span>Robert Downey Jr.</span>
+            </li>
+            <li>
+                <img src="public/images/actors/emma-stone.jpg" alt="Emma Stone">
+                <span>Emma Stone</span>
+            </li>
+        </ul>
+        <a href="#" class="discover-more">
+            <span>Découvrir d'autres acteurs</span>
+            <i class="fa-solid fa-arrow-right"></i>
+        </a>
+    </div>
 </aside>
